ce464323b8e5e175a5323d5b83f815317719f24c
lapistar <lapistar@aliyun.com>
2019-03-11 19:57:01 +0800

    第一个测试项目

--- src/main/java/cn/itGit/HelloWorldTest.java
+++ src/main/java/cn/itGit/HelloWorldTest.java
@@ -1,7 +1,18 @@
 package cn.itGit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("welcome to java!");
+        List<Integer> integers = new ArrayList<Integer>(3);
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        for (Integer integer : integers) {
+            System.out.println(integer);
+        }
+
     }
 }
