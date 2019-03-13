e0a3f05db1064623f4648eef57e001ea45afe5e8
lapistar <lapistar@aliyun.com>
2019-03-13 10:23:08 +0800

    数组内元素指针,指向对象

--- src/main/java/cn/itGit/JvmTest.java
+++ src/main/java/cn/itGit/JvmTest.java
@@ -1,11 +1,38 @@
 package cn.itGit;
 
+import java.util.LinkedList;
+
 public class JvmTest {
 
     public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().maxMemory()/1024/1024);
-        System.out.println(Runtime.getRuntime().freeMemory()/1024/1024);
-        System.out.println(Runtime.getRuntime().totalMemory()/1024/1024);
+        System.out.println(Runtime.getRuntime().maxMemory() / 1024 / 1024);
+        System.out.println(Runtime.getRuntime().freeMemory() / 1024 / 1024);
+        System.out.println(Runtime.getRuntime().totalMemory() / 1024 / 1024);
 
+        LinkedList<String> ll = new LinkedList<String>();
+        int[] arr = new int[5];
+        Student[] stus = new Student[4];
+        stus[0] = new Student("zhang3", 18);
+        stus[1] = new Student("li4", 19);
+        stus[2] = new Student("wang5", 20);
+        stus[3] = new Student();
+
+        for (Student student : stus) {
+            System.out.println(student.name+" --> "+student.age);
+        }
+
+    }
+
+    static class Student {
+        private String name;
+        private int age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Student() {
+        }
     }
 }
