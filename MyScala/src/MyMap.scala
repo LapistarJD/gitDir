8618949886efe5d12c892d629fd230f58217a958
lapistar <lapistar@aliyun.com>
2019-03-18 18:06:18 +0800

    scala 测试4 Set MutMap ImmutMap

--- MyScala/src/MyMap.scala
+++ MyScala/src/MyMap.scala
@@ -1,20 +1,131 @@
-import scala.collection.mutable.Map
-
-object MyMap {
+object MyImmutMap {
   def main(args: Array[String]): Unit = {
-    val map = Map("赵敏" -> 19, "周芷若" -> 18, ("张无忌", 21))
+    //定义Map集合
+    //1.val map=Map(键 -> 值 , 键 -> 值...)
+    //2.利用元组构建  val map=Map((键，值), (键，值) , (键，值)....)
+    val map1 = Map("zhangsan" -> 30, "lisi" -> 40)
+    val map2 = Map(("zhang3", 31), ("li4", 41))
 
-    map += ("宋青书" -> 25, ("小昭", 22))
 
-    map("张无忌") = 100
+    val map3: Map[String, Int] = map1 ++ map2
+    map3.map(x => println(x._1 + " -><- " + x._2))
+    //lisi -><- 40
+    //zhangsan -><- 30
+    //zhang3 -><- 31
+    //li4 -><- 41
+    val map4: Map[String, Int] = map1 + (("wangwu", 50))
+    map4.map(x => println(x._1 + " #### " + x._2))
+    //zhangsan #### 30
+    //lisi #### 40
+    //wangwu #### 50
 
-    for (elem <- map) {
-      println(elem)
+    val map5: Map[String, Int] = map1 - "zhangsan"
+    map5.map(x => println(x._1 + " ---- " + x._2))
+    //lisi ---- 40
+
+    val map6: Map[String, Int] = map1 -- Array("zhangsan","lisi")
+    println(map6.size)   // 0
+
+    //操作map集合
+    //获取值： 值=map(键)
+    println("zhangsan -> " + map1("zhangsan")) //zhangsan -> 30
+    println("li4 -> " + map2("li4")) //li4 -> 41
+    println("wang5 -> " + map2.get("wang5")) //wang5 -> None
+    println("wang5 -> " + map2.getOrElse("wang5", 51)) //wang5 -> 51
+    //原则：通过先获取键，在获取键对应值。
+
+    //遍历map集合
+    //方法一：显示所有的key
+    val keys: Iterable[String] = map1.keys
+    for (elem <- keys) {
+      println(elem + " -> " + map1(elem))
+      //lisi -> 40
+      //zhangsan -> 30
     }
-//    (宋青书,25)
-//    (周芷若,18)
-//    (小昭,22)
-//    (张无忌,100)
-//    (赵敏,19)
+
+    //方法二：显示所有的key
+    val keySet: collection.Set[String] = map2.keySet
+    for (elem <- keySet) {
+      println(elem + " -> " + map2(elem))
+      //zhang3 -> 31
+      //li4 -> 41
+    }
+
+  }
+}
+
+//导包
+import scala.collection.mutable._
+object MyMutMap{
+  def main(args: Array[String]): Unit = {
+
+
+    //声明一个可变集合
+    val user =HashMap("zhangsan"->50,"lisi" -> 100)
+    val stu =HashMap("wang5"->51,"zhao6" -> 91)
+
+    user ++= stu
+    //添加键值对
+    user +=("wangwu" -> 30)
+
+    //添加多个键值对
+    user += ("zhangsan0" -> 30,"lisi0" -> 20)
+    println(user.size)    // 7
+    user.map(x=>println(x._1+"    "+x._2))
+    //    zhangsan0    30
+    //    lisi    100
+    //    wang5    51
+    //    zhangsan    50
+    //    lisi0    20
+    //    zhao6    91
+    //    wangwu    30
+
+    //方法一：显示所有的key
+    user.keys
+
+    //方法二：显示所有的key
+    user.keySet
+
+    //通过key获取value
+    user("zhangsan")
+
+    //通过key获取value 有key对应的值则返回，没有就返回默认值0，
+    user.getOrElse("zhangsan",0)
+
+    //没有对应的key，返回默认0
+    user.getOrElse("zhangsan1",0)
+
+    //更新键值对
+    user("zhangsan") = 55
+    user("zhangsan")
+
+    //更新多个键值对
+    user += ("zhangsan" -> 60, "lisi" -> 50)
+
+    //删除key
+    user -=("zhangsan")
+
+    //删除key
+    user.remove("zhangsan0")
+
+    user--=Array("lisi0","lisi","wangwu")
+
+    //遍历map 方法一：通过key值
+    for(x<- user.keys) println(x+" -> "+user(x))
+    //wang5 -> 51
+    //zhao6 -> 91
+
+    //遍历map 方法二：模式匹配
+    for((x,y) <- user) println(x+" -> "+y)
+    //wang5 -> 51
+    //zhao6 -> 91
+
+    //遍历map 方法三：通过foreach
+    user.foreach{case (x,y) => println(x+" -> "+y)}
+    //wang5 -> 51
+    //zhao6 -> 91
+
+
+
   }
 }
---
+++ MyScala/src/MySet.scala
@@ -0,0 +1,124 @@
+object MySet {
+  def main(args: Array[String]): Unit = {
+    //定义一个不可变的Set集合
+    //定义：val set=Set(元素,元素,.....)
+
+    val set = Set(1, 2, 3, 4, 5, 6, 7)
+    println(set)
+    //Set(5, 1, 6, 2, 7, 3, 4)
+
+    //元素个数
+    val size: Int = set.size
+    println(size)
+    //7
+
+    //取集合最小值
+    val min: Int = set.min
+    println(min)
+    //1
+
+    //取集合最大值
+    val max: Int = set.max
+    println(max)
+    //7
+
+    //将元素和set1合并生成一个新的set，原有set不变
+    val set2: Set[Int] = set + 8
+    println(set) //Set(5, 1, 6, 2, 7, 3, 4)
+    println(set2) //Set(5, 1, 6, 2, 7, 3, 8, 4)
+
+
+    val set1 = Set(7, 8, 9)
+    println(set1)
+    //Set(7, 8, 9)
+
+    //两个集合的交集
+    val set3: Set[Int] = set & set1
+    println(set) //Set(5, 1, 6, 2, 7, 3, 4)
+    println(set1) //Set(7, 8, 9)
+    println(set3) //Set(7)
+
+    //两个集合的并集
+    val set4: Set[Int] = set ++ set1
+    println(set4)
+    //Set(5, 1, 6, 9, 2, 7, 3, 8, 4)
+
+    //在第一个set基础上去掉第二个set中存在的元素
+    val set5: Set[Int] = set -- set1
+    println(set5)
+    //Set(5, 1, 6, 2, 3, 4)
+
+    //返回第一个不同于第二个set的元素集合
+    val set6: Set[Int] = set &~ set1
+    println(set6)
+    //Set(5, 1, 6, 2, 3, 4)
+
+    //计算符合条件的元素个数
+    val count: Int = set.count(_ > 5)
+    println(count) //2
+
+    //返回第一个不同于第二个的元素集合
+    val set7: Set[Int] = set.diff(set1)
+    println(set7)
+    //Set(5, 1, 6, 2, 3, 4)
+
+    //返回第一个不同于第二个的元素集合
+    val set8: Set[Int] = set1.diff(set)
+    println(set8)
+    //Set(8, 9)
+
+    //取子set(2,5为元素位置, 从0开始，包含头不包含尾)
+    val set9: Set[Int] = set.slice(2, 5)
+    println(set9)
+    //Set(6, 2, 7)
+
+    //迭代所有的子set，取指定的个数组合
+    set1.subsets(2).foreach(x => println(x))
+    //Set(7, 8)
+    //Set(7, 9)
+    //Set(8, 9)
+
+  }
+
+}
+
+//导入可变Set包
+import scala.collection.mutable._
+
+object MyMutSet {
+  def main(args: Array[String]): Unit = {
+
+    //定义一个可变的Set
+    val set1 = new HashSet[Int]()
+    val set2 = HashSet[Int](1, 2, 3)
+
+
+    //添加元素
+    set1 += 1
+    println(set1)   //Set(1)
+
+    //添加元素  add等价于+=
+    set1.add(2)
+    println(set1)   //Set(1,2)
+
+
+    //向集合中添加元素集合
+    set1 ++= Set(1, 4, 5)
+    println(set1)   //Set(1, 5, 2, 4)
+
+    //删除一个元素
+    set1 -= 5
+    println(set1)   //Set(1, 2, 4)
+
+    //删除一个元素
+    set1.remove(1)
+    println(set1)   //Set(2, 4)
+
+    //返回第一个不同于第二个set的元素集合
+    val set3: HashSet[Int] = set1 &~ set2
+    println(set3)   //Set(4)
+    val set4: HashSet[Int] = set2 &~ set1
+    println(set4)   //Set(1, 3)
+
+  }
+}
